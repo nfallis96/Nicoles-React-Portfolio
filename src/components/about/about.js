d8d981c9b8217928d118af8d02fd8344238d9326
nfallis96 <nfallis2019@fau.edu>
2023-03-20 23:50:29 -0400

    updates

---
+++ src/components/about/about.js
@@ -0,0 +1,98 @@
+function About() {
+  return (
+    <div className="container aboutSkills">
+      <div className="container about">
+        <h1>About Me</h1>
+        <div className="aboutMe">
+          <p>
+            My name is Nicole Fallis, I'm from florida, and I'm a Pre-med student who 
+            took a software development bootcamp for fun because I was interested in it.
+             I am completing my bachelor's degree in biology and psychology at the Florida 
+             Atlantic University and I've learned so much from this Full Stack Coding Bootcamp, 
+             and have had the pleasure of developing applications from business platforms to social media
+            platforms.
+          </p>
+        </div>
+      </div>
+      <div className="container skills">
+        <h1> The Tools </h1>/
+        <div className="">
+          <ul className="">
+            <li>
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+            </li>
+          </ul>
+          <ul className="">
+            <li>
+              <img
+                alt="m"
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+            </li>
+          </ul>
+          <ul className="">
+            <li>
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+              <img
+                alt=""
+                src=""
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
